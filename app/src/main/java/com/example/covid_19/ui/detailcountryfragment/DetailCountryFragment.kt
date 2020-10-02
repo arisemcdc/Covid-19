da55f0c127b3617dd2636acbe96b4033f08338ad
Arisemcdc <arisemcdc1@gmail.com>
2020-10-02 23:15:59 +0300

    добавил стрелку home

--- app/src/main/java/com/example/covid_19/ui/detailcountryfragment/DetailCountryFragment.kt
+++ app/src/main/java/com/example/covid_19/ui/detailcountryfragment/DetailCountryFragment.kt
@@ -2,13 +2,12 @@ package com.example.covid_19.ui.detailcountryfragment
 
 import androidx.lifecycle.ViewModelProviders
 import android.os.Bundle
+import android.view.*
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import android.widget.Toast
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
+import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
 import com.example.covid_19.R
 import com.example.covid_19.data.DataResult
@@ -38,7 +37,8 @@ class DetailCountryFragment : Fragment() {
 
 
         root=inflater.inflate(R.layout.detail_country_fragment, container, false)
-        //root.nav_view.visibility = View.GONE
+        setHasOptionsMenu(true)
+        activity?.nav_view?.visibility = View.GONE
         viewModel.country.observe(viewLifecycleOwner, Observer{country ->
             if (country != null){
                 root.countryNameValue.setText(country.name)
@@ -78,6 +78,22 @@ class DetailCountryFragment : Fragment() {
         return root
     }
 
+    /*override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater.inflate(R.menu.back_menu, menu)
+    }*/
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            android.R.id.home -> {
+                findNavController().popBackStack()
+            }
+           /* R.id.backItem -> {
+                findNavController().popBackStack()
+            }*/
+        }
+        return true
+    }
    /* override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
         viewModel = ViewModelProviders.of(this).get(DetailCountryViewModel::class.java)
